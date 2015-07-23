6dee1cf3ef7444b48119446f400059450a984cce
dyrnade <gurescicem@gmail.com>
2015-07-23 17:55:40 +0300

    zato server.conf

---
+++ server_conf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CPU_PROCESSORS=$(cat /proc/cpuinfo | grep "cpu cores" | wc -l)
+
+CPU_CORES=$(grep "cpu cores" /proc/cpuinfo |sort -u |cut -d":" -f2)
+
+WORKERS=$((CPU_CORES * CPU_PROCESSORS* 2 + 1))
+
+SERVERCONF=~/aa/server/config/repo/server.conf
+
+sed -i 's/.*gunicorn_bind=localhost:..*/gunicorn_bind=0.0.0.0:17010/' $SERVERCONF
+
+sed -i "s/.*gunicorn_workers=.*/gunicorn_workers=$WORKERS/p" $SERVERCONF
