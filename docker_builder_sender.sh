c1a465cd130387bdd708a5175e42a63bfc09f224
dyrnade <gurescicem@gmail.com>
2015-09-06 02:59:07 +0300

    first try

---
+++ docker_builder_sender.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ar=/home/cemg/projects/zcloud/containers/
+
+cd $ar
+
+for i in $(ls $ar)
+do
+
+if [ -d $i ]
+then
+
+cd $i
+
+wait $(sudo docker build -t zetaops/$i --file=$(pwd)/Dockerfile $(pwd))
+echo $(pwd)
+fi
+
+cd $ar
+echo $(pwd)
+done
+
